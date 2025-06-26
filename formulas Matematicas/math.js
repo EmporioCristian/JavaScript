fb19eedcf5879fd0de3e934299ef2c9505a190c7
dafekk <dafekk4@gmail.com>
2025-06-26 16:25:24 -0500

    Se agragaron funciones para sacar diametro y area de un circulo

--- formulas Matematicas/math.js
+++ formulas Matematicas/math.js
@@ -45,3 +45,22 @@ function calcularTriangulo(lado1, lado2, base, altura){
     }
 }
 console.groupEnd('Triangulo')
+
+console.group('Circulo')
+
+const radioCirculo = 3;
+const diametroCirculo =  radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo + PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo,
+})
+
+console.groupEnd('Circulo')
